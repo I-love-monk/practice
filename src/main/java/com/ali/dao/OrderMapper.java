df31d01722faec1ca13a4e8f9f1f6a9c04c385b5
I-love-monk <I-love-monk@live.com>
2017-11-23 18:19:10 +0800

    first commit

---
+++ src/main/java/com/ali/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.ali.dao;
+
+import com.ali.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
---
+++ src/main/java/com/ali/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.ali.dao;
+
+import com.ali.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
